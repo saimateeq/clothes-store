b8cd1c331eedbd6decd9d9a8906e6ef0cbbce327
Saim Ateeq <143260869+saimateeq@users.noreply.github.com>
2026-08-19 13:24:52 -0700

    Update CORS origin to use environment variable

--- server/app.js
+++ server/app.js
@@ -15,7 +15,7 @@ app.set("trust proxy", 1);
 app.use(helmet());
 app.use(
   cors({
-    origin: process.env.CLIENT_URL || "http://localhost:5173",
+    origin: process.env.CLIENT_URL,
     credentials: true,
   })
 );
